563eda120af8d7d70f958a7218bde987d7076f32
Drysdale Chivhanga <chivhangadrys@gmail.com>
2022-06-18 18:17:37 +0200

    first commit

---
+++ standard-query-response/Generic-Messgase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drysdale.Standard.Query.Response
+{
+    /// <summary>
+    /// Class.Message
+    /// </summary>
+    public class Message
+    {
+        /// <summary>
+        /// AddSuccess
+        /// </summary>
+        /// <param name="modelName"></param>
+        /// <returns></returns>
+        public static string AddSuccess(string modelName) => $"{modelName}, Added Successfully";
+
+        /// <summary>
+        /// QuerySuccess
+        /// </summary>
+        /// <param name="modelName"></param>
+        /// <returns></returns>
+        public static string QuerySuccess(string modelName) => $"Query On {modelName},  Successful";
+    }
+}
---
+++ standard-query-response/Query-Response.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drysdale.Standard.Query.Response
+{
+    /// <summary>
+    /// Query.Response
+    /// </summary>
+    public class QueryResponse
+    {
+        /// <summary>
+        /// Code
+        /// </summary>
+        public int Code { get; set; } = 0;
+
+        /// <summary>
+        /// Message
+        /// </summary>
+        public string Message { get; set; } = "Tech-Error Detected!";
+    }
+}
