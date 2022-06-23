19c28498c602cebe200b05d927394ec41f74d91a
Drysdale Chivhanga <chivhangadrys@gmail.com>
2022-06-23 21:15:32 +0200

    Added Row Request Status

---
+++ standard-query-response/RowRequestStatus.cs
@@ -0,0 +1,25 @@
+﻿namespace Drysdale.Standard.Query.Response
+{
+    /// <summary>
+    /// Enumeration.RowRequestStatus
+    /// </summary>
+    public enum RowRequestStatus
+    {
+        /// <summary>
+        /// Ignore (Select All)
+        /// </summary>
+        Ignore = -86,
+        /// <summary>
+        /// NotActive
+        /// </summary>
+        NotActive = -1,
+        /// <summary>
+        /// Pending
+        /// </summary>
+        Pending,
+        /// <summary>
+        /// Active
+        /// </summary>
+        Active
+    }
+}
