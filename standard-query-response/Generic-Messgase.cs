e603a2a6148dea1be1ecd64e28b569252f7a5ca0
Drysdale Chivhanga <chivhangadrys@gmail.com>
2022-07-03 15:28:26 +0200

    Added Generic Technical Error Message

--- standard-query-response/Generic-Messgase.cs
+++ standard-query-response/Generic-Messgase.cs
@@ -5,18 +5,41 @@
     /// </summary>
     public class Message
     {
+        #region AddSuccess...
         /// <summary>
         /// AddSuccess
         /// </summary>
         /// <param name="modelName"></param>
         /// <returns></returns>
         public static string AddSuccess(string modelName) => $"{modelName}, Added Successfully";
+        #endregion
 
+        #region QuerySuccess...
         /// <summary>
         /// QuerySuccess
         /// </summary>
         /// <param name="modelName"></param>
         /// <returns></returns>
         public static string QuerySuccess(string modelName) => $"Query On {modelName},  Successful";
+        #endregion
+
+        #region DuplcateError...
+        /// <summary>
+        /// DuplcateError
+        /// </summary>
+        /// <param name="modelName"></param>
+        /// <returns></returns>
+        public static string DuplcateError(string modelName) => $"Duplicate {modelName}, Detected!";
+        #endregion
+
+        #region TechError...
+        /// <summary>
+        /// TechError
+        /// </summary>
+        /// <param name="functionName"></param>
+        /// <param name="messageDetails"></param>
+        /// <returns>The Tecnical Error Message</returns>
+        public static string TechError(string functionName, string messageDetails) => $"{functionName}, Detected! [{messageDetails}]";
+        #endregion
     }
 }
